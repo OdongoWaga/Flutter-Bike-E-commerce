1aad577bfb723f0b1b3d8d63b1817a0148661c36
Waga <wagaodongo@gmail.com>
2019-08-25 22:47:32 +0300

    redux changes

--- lib/main.dart
+++ lib/main.dart
@@ -2,30 +2,46 @@ import 'package:flutter/material.dart';
 import 'package:flutter_ecommerce/pages/login.dart';
 import 'package:flutter_ecommerce/pages/products_page.dart';
 import 'package:flutter_ecommerce/pages/register.dart';
+import 'package:flutter_ecommerce/redux/reducers.dart';
+import 'package:redux/redux.dart';
+import 'models/app_state.dart';
+import 'package:redux_thunk/redux_thunk.dart';
+import 'package:flutter_redux/flutter_redux.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  final store = Store<AppState>(appReducer,
+      initialState: AppState.initial(), middleware: [thunkMiddleware]);
+  runApp(MyApp(store: store));
+}
 
 class MyApp extends StatelessWidget {
+  final Store<AppState> store;
+
+  MyApp({this.store});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Soko E-commerce',
-      routes: {
-        '/login': (BuildContext context) => LoginPage(),
-        '/register': (BuildContext context) => RegisterPage(),
-        '/products': (BuildContext context) => ProductsPage()
-      },
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        primaryColor: Colors.cyan[400],
-        accentColor: Colors.deepOrange[200],
-        textTheme: TextTheme(
-          headline: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
-          title: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
-          body1: TextStyle(fontSize: 18.0),
+    return StoreProvider(
+      store: store,
+      child: MaterialApp(
+        title: 'Soko E-commerce',
+        routes: {
+          '/login': (BuildContext context) => LoginPage(),
+          '/register': (BuildContext context) => RegisterPage(),
+          '/products': (BuildContext context) => ProductsPage()
+        },
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          primaryColor: Colors.cyan[400],
+          accentColor: Colors.deepOrange[200],
+          textTheme: TextTheme(
+            headline: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+            title: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+            body1: TextStyle(fontSize: 18.0),
+          ),
         ),
+        home: RegisterPage(),
       ),
-      home: ProductsPage(),
     );
   }
 }
---
+++ lib/redux/reducers.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_ecommerce/models/app_state.dart';
+
+AppState appReducer(state, action) {
+  return AppState(user: userReducer(state.user, action));
+}
+
+userReducer(user, action) {
+  return user;
+}

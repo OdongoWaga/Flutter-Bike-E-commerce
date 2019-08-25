d8b0bfec0b719381d032fb771bbb79afda9a68cc
Waga <wagaodongo@gmail.com>
2019-08-25 21:17:29 +0300

    succesful login.

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class LoginPage extends StatefulWidget {
   @override
@@ -6,7 +9,9 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  bool _obscureText = true;
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  bool _isSubmitting, _obscureText = true;
 
   final _formKey = GlobalKey<FormState>();
 
@@ -69,23 +74,28 @@ class _LoginPageState extends State<LoginPage> {
       padding: EdgeInsets.only(top: 20.0),
       child: Column(
         children: <Widget>[
-          RaisedButton(
-            child: Text(
-              'Submit',
-              style: Theme.of(context)
-                  .textTheme
-                  .body1
-                  .copyWith(color: Colors.black),
-            ),
-            elevation: 8.0,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(
-                Radius.circular(10.0),
-              ),
-            ),
-            color: Theme.of(context).accentColor,
-            onPressed: _submit,
-          ),
+          _isSubmitting == true
+              ? CircularProgressIndicator(
+                  valueColor:
+                      AlwaysStoppedAnimation(Theme.of(context).accentColor),
+                )
+              : RaisedButton(
+                  child: Text(
+                    'Submit',
+                    style: Theme.of(context)
+                        .textTheme
+                        .body1
+                        .copyWith(color: Colors.black),
+                  ),
+                  elevation: 8.0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10.0),
+                    ),
+                  ),
+                  color: Theme.of(context).accentColor,
+                  onPressed: _submit,
+                ),
           FlatButton(
             child: Text('New User? Register'),
             onPressed: () =>
@@ -100,13 +110,75 @@ class _LoginPageState extends State<LoginPage> {
     final form = _formKey.currentState;
     if (form.validate()) {
       form.save();
-      print(' Email: $_email, Password: $_password');
+      _registerUser();
     }
+  }
+
+  void _registerUser() async {
+    setState(() {
+      _isSubmitting = true;
+    });
+    http.Response response = await http.post(
+      'http://localhost:1337/auth/local',
+      body: {
+        'identifier': _email,
+        'password': _password,
+      },
+    );
+    final responseData = json.decode(response.body);
+
+    if (response.statusCode == 200) {
+      setState(() {
+        _isSubmitting = false;
+      });
+      _showSuccessSnack();
+
+      _redirectUser();
+      print(responseData);
+    } else {
+      setState(() {
+        _isSubmitting = false;
+      });
+      final String errorMsg = responseData['message'];
+
+      _showErrorSnack(errorMsg);
+    }
+  }
+
+  void _showSuccessSnack() {
+    final snackbar = SnackBar(
+        content: Text(
+      'User succesfully logged in',
+      style: TextStyle(color: Colors.green),
+    ));
+    _scaffoldKey.currentState.showSnackBar(snackbar);
+    _formKey.currentState.reset();
+  }
+
+  void _showErrorSnack(String errorMsg) {
+    final snackbar = SnackBar(
+      content: Text(
+        errorMsg,
+        style: TextStyle(color: Colors.red),
+      ),
+    );
+    _scaffoldKey.currentState.showSnackBar(snackbar);
+    throw Exception('Error logging in:  $errorMsg');
+  }
+
+  void _redirectUser() {
+    Future.delayed(
+      Duration(seconds: 2),
+      () {
+        Navigator.pushReplacementNamed(context, '/products');
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         title: Text('Login'),
       ),
